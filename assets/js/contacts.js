eb0366f08001eb05e7f7bd8b73c9c67ecd4d762c
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-04-17 22:51:37 +0300

    Update contacts.js

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -9,8 +9,8 @@ async function renderBitumContacts() {
     titleContacts.innerHTML = bitum.navigation.contacts.titleContacts;
 
     const contactsBlock = document.querySelector('#contactsBlock');
-    contactsBlock.innerHTML = `<p class='content-contacts'>${bitum.contacts.text}</p>`;
-    contactsBlock.innerHTML += `<p class='content-contacts'><a href='mailto:${bitum.contacts.mail}'><img src='./assets/img/icon-gmail.png' alt='Gmail' class='img-gmail'> ${bitum.contacts.mail}</a></p>`;
+    contactsBlock.innerHTML = `<p class='text-center'>${bitum.contacts.text}</p>`;
+    contactsBlock.innerHTML += `<p class='text-center'><a href='mailto:${bitum.contacts.mail}'><img src='./assets/img/icon-gmail.png' alt='Gmail' class='img-gmail'> ${bitum.contacts.mail}</a></p>`;
     contactsBlock.innerHTML += `<iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d84322.05769903812!2d32.99253113533931!3d48.66601294400255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d09d92b326f807%3A0xe117d4fe41dc0d9!2z0JDQu9C10LrRgdCw0L3QtNGA0LjRjywg0JrQuNGA0L7QstC-0LPRgNCw0LTRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwgMjgwMDE!5e0!3m2!1sru!2sua!4v1681560460738!5m2!1sru!2sua"
                                 width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy"
