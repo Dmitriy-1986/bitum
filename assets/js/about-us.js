1848d1158abd600fb162b67954ae6d9846789dd4
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-04-20 16:48:54 +0300

    Update about-us.js

--- assets/js/about-us.js
+++ assets/js/about-us.js
@@ -8,7 +8,7 @@ async function renderBitumAboutUs() {
     // About Us
     const breadcrumbAboutUs = document.querySelector('#breadcrumbAboutUs');
     breadcrumbAboutUs.innerHTML = `<ol class="breadcrumb">
-                                       <li class="breadcrumb-item"><a href="${bitum.navigation.home.linkHome}">Головна</a></li>
+                                       <li class="breadcrumb-item"><a href="${bitum.navigation.home.linkHome}">Головна </a></li>
                                        <li class="breadcrumb-item active">Про Нас</li>
                                    </ol>`;
     
