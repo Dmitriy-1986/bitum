419538d684e17dc91b864ea1499824276044151d
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-04-20 17:01:39 +0300

    Update music.js

--- assets/js/music.js
+++ assets/js/music.js
@@ -5,6 +5,12 @@ async function renderBitumMusic() {
 
     document.title = `${bitum.groupName} | ${bitum.navigationMenu[2].title}`;
     
+    const breadcrumbMusic = document.querySelector('#breadcrumbMusic');
+    breadcrumbMusic.innerHTML = `<ol class="breadcrumb">
+                                       <li class="breadcrumb-item"><a href="${bitum.navigation.home.linkHome}">${bitum.navigation.home.titleHome} </a></li>
+                                       <li class="breadcrumb-item active">${bitum.navigation.music.titleMusic}</li>
+                                   </ol>`;
+    
     const titleMusic = document.querySelector('#titleMusic');
     titleMusic.innerHTML = bitum.navigation.music.titleMusic;
     
