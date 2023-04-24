b80179c8508ba18ac9c9c730efd7f20e1ba92e47
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-04-24 11:42:24 +0300

    Update header-navigation.js

--- assets/js/header-navigation.js
+++ assets/js/header-navigation.js
@@ -29,13 +29,21 @@ async function renderBitumHeaderNavigation() {
     logo.innerHTML += `<a href="https://bitum.pp.ua"><img src='./assets/img/ukraine-flag.png' alt='Ukraine' class='flag-Ukraine'></a>`;
 
     const navList = document.querySelector('#navList');
+    navList.innerHTML  =  `<li><a href='${navigation.home.linkHome}'>${navigation.home.titleHome}</a></li>`;
+    navList.innerHTML +=  `<li><a href='${navigation.aboutUs.linkAboutUs}'>${navigation.aboutUs.titleAboutUs}</a></li>`;
+    navList.innerHTML +=  `<li><a href='${navigation.archive.linkArchive}'>${navigation.archive.titleArchive}</a></li>`;
+    navList.innerHTML +=  `<li><a href='${navigation.gallery.linkGallery}'>${navigation.gallery.titleGallery}</a></li>`;
+    navList.innerHTML +=  `<li><a href='${navigation.music.linkMusic}'>${navigation.music.titleMusic}</a></li>`;
+    navList.innerHTML +=  `<li><a href='${navigation.contacts.linkContacts}'>${navigation.contacts.titleContacts}</a></li>`;
+    
+    /*
     let html = '';
     bitum.navigationMenu.forEach(nav => {
         let htmlSegment = `<li><a href='${nav.link}'>${nav.title}</a></li>`;
         html += htmlSegment;
-        console.log(html);
     });
     navList.innerHTML = html;
+    */
 }
 
 renderBitumHeaderNavigation();
