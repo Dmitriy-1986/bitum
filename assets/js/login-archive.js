6960daa570ff7f6a57a3bd92f116ee6c539520c0
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-04-29 22:29:28 +0300

    Update login-archive.js

--- assets/js/login-archive.js
+++ assets/js/login-archive.js
@@ -7,7 +7,14 @@ btnArchive.addEventListener('click', (e) => {
   e.preventDefault();
   if(passwordArchive.value === '111') {
     formArchive.style.display = 'none';
-    result.innerHTML = '<b class="password-valid">Вітаю, ви увійшли в Архів</b>'
+    result.innerHTML = `<p class="password-valid">Вітаю, ви увійшли в Архів 
+                          <b id="logOut" class="log-out"> Вийти </b></p>`;
+    
+      const logOut = document.querySelector('#logOut');
+      logOut.addEventListener('click', () => {
+          location.reload()
+      });
+    
   } else {
     result.innerHTML = '<b class="password-error">Ви ввели неправильний пароль</b>';
   }
