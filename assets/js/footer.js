8110bcf05050691d709f4be2a030ac66ea513f13
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-04-29 21:28:27 +0300

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -7,6 +7,8 @@ async function renderBitumFooter() {
     footerContent.innerHTML  = `Copyright &copy `; 
     footerContent.innerHTML += new Date().getFullYear();
     footerContent.innerHTML += ` ${bitum.groupName}`;
+    footerContent.innerHTML += ` <a href="mailto:bitum2003@gmail.com" title="Gmail">
+                                    <img src="./assets/img/icon-gmail.png" alt="Gmail" class="img-gmail"></a>`;
 }
 
 renderBitumFooter();
