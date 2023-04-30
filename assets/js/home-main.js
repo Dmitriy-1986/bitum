82ecc662d262651137be59467bbbfad64299388a
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-04-30 09:25:44 +0300

    Update home-main.js

--- assets/js/home-main.js
+++ assets/js/home-main.js
@@ -65,10 +65,14 @@ async function renderBitumHomeMain() {
     // Contacts
     const contactTitle = document.querySelector('#contactTitle');
     const contentContacts = document.querySelector('#contentContacts p');
+    const googleMaps = document.querySelector('#googleMaps');
+    
     contactTitle.innerHTML = `<h2>${bitum.contacts.title}</h2>`;
     contentContacts.innerHTML = `<p class='content-about-us'>${bitum.contacts.text}</p>`;
     contentContacts.innerHTML += `<a href='mailto:${bitum.contacts.mail}'><img src='./assets/img/icon-gmail.png' alt='Gmail' class='img-gmail'> ${bitum.contacts.mail}</a>`;
-
+    googleMaps.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d104008.81614515168!2d33.07541334587099!3d48.66423519437349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d09d92b326f807%3A0xe117d4fe41dc0d9!2z0J7Qu9C10LrRgdCw0L3QtNGA0ZbRjywg0JrRltGA0L7QstC-0LPRgNCw0LTRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgMjgwMDE!5e0!3m2!1suk!2sua!4v1681932280382!5m2!1suk!2sua" 
+                                     height="350" style="border:0;" allowfullscreen="" loading="lazy" 
+                                    referrerpolicy="no-referrer-when-downgrade"></iframe>`;
 }
 
 renderBitumHomeMain();
