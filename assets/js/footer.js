95eeb1b834bb615328eaf1a77054fa334d89b8e6
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-02 16:23:50 +0300

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -7,27 +7,27 @@ async function renderBitumFooter() {
     btnShare.innerHTML = `<h3>Поділитись сторінкою</h3>`;
     btnShare.innerHTML += `<ul class="share">
                                 <li class="social-btn">
-                                    <a class="social-link" href="https://www.facebook.com/sharer.php?u=${window.location.href}">
+                                    <a class="social-link" title="Facebook" href="https://www.facebook.com/sharer.php?u=${window.location.href}">
                                         <img class="icon-facebook" src="./assets/img/social-icon/facebook.svg" alt="Facebook">
                                     </a>
                                 </li>
                                 <li class="social-btn">
-                                    <a class="social-link" href="https://twitter.com/intent/tweet?text=${window.location.href}">
+                                    <a class="social-link" title="Twitter" href="https://twitter.com/intent/tweet?text=${window.location.href}">
                                         <img class="icon-twitter" src="./assets/img/social-icon/twitter.svg" alt="Twitter">
                                     </a>
                                 </li>
                                 <li class="social-btn">
-                                    <a class="social-link" href="https://telegram.me/share/url?url=${window.location.href}">
+                                    <a class="social-link" title="Telegram" href="https://telegram.me/share/url?url=${window.location.href}">
                                         <img class="icon-telegram" src="./assets/img/social-icon/telegram.svg" alt="Telegram">
                                     </a>
                                 </li>
                                 <li class="social-btn">
-                                    <a class="social-link" href="https://api.whatsapp.com/send?text=${window.location.href}">
+                                    <a class="social-link" title="WhatsApp" href="https://api.whatsapp.com/send?text=${window.location.href}">
                                         <img class="icon-whatsapp" src="./assets/img/social-icon/whatsapp.svg" alt="Whatsapp">
                                     </a>
                                 </li>
                                 <li class="social-btn">
-                                    <a class="social-link" href="viber://forward?text=${window.location.href}">
+                                    <a class="social-link" title="Viber" href="viber://forward?text=${window.location.href}">
                                         <img class="icon-viber" src="./assets/img/social-icon/viber.svg" alt="Whatsapp">
                                     </a>
                                 </li>
