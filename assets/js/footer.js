f7f81e675678dd2a8bae9cb2f9f8270e4d66fcaa
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-02 14:23:35 +0300

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -4,6 +4,36 @@ async function renderBitumFooter() {
     const bitum = await getBitum();
 
     const footerContent = document.querySelector('#footerContent');
+    
+    const btnShare = document.createElement('div');
+    btnShare.innerHTML = `<ul class="share">
+                                <li class="social-btn">
+                                    <a class="social-link" href="https://www.facebook.com/sharer.php?u=${window.location.href}">
+                                        <img class="icon-facebook" src="./socia-icon/facebook.svg" alt="Facebook">
+                                    </a>
+                                </li>
+                                <li class="social-btn">
+                                    <a class="social-link" href="https://twitter.com/intent/tweet?text=${window.location.href}">
+                                        <img class="icon-twitter" src="./socia-icon/twitter.svg" alt="Twitter">
+                                    </a>
+                                </li>
+                                <li class="social-btn">
+                                    <a class="social-link" href="https://telegram.me/share/url?url=${window.location.href}">
+                                        <img class="icon-telegram" src="./socia-icon/telegram.svg" alt="Telegram">
+                                    </a>
+                                </li>
+                                <li class="social-btn">
+                                    <a class="social-link" href="https://api.whatsapp.com/send?text=${window.location.href}">
+                                        <img class="icon-whatsapp" src="./socia-icon/whatsapp.svg" alt="Whatsapp">
+                                    </a>
+                                </li>
+                                <li class="social-btn">
+                                    <a class="social-link" href="viber://forward?text=${window.location.href}">
+                                        <img class="icon-viber" src="./socia-icon/viber.svg" alt="Whatsapp">
+                                    </a>
+                                </li>
+                            </ul>`;
+    footerContent.append(btnShare);
     footerContent.innerHTML  = `Copyright &copy `; 
     footerContent.innerHTML += new Date().getFullYear();
     footerContent.innerHTML += ` ${bitum.groupName}`;
