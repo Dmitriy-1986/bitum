4cdc3f98ef1f090d78106dc45cbaf8b87e4f9644
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-09 16:36:31 +0300

    Update music.js

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,18 +1,1 @@
-'use strict';
 
-async function renderBitumMusic() {
-    const bitum = await getBitum();
-
-    document.title = `${bitum.groupName} | ${bitum.navigationMenu[2].title}`;
-    
-    const breadcrumbMusic = document.querySelector('#breadcrumbMusic');
-    breadcrumbMusic.innerHTML = `<ol class="breadcrumb">
-                                       <li class="breadcrumb-item"><a href="${bitum.navigation.home.linkHome}">${bitum.navigation.home.titleHome} </a></li>
-                                       <li class="breadcrumb-item active">${bitum.navigation.music.titleMusic}</li>
-                                   </ol>`;
-    
-    const titleMainMusic = document.querySelector('#titleMainMusic');
-    titleMainMusic.innerHTML = `<h2>${bitum.navigation.music.titleMusic}</h2>`;  
-}
-
-renderBitumMusic();
