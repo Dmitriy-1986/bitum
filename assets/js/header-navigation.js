4211873ed96e590dea7cc5e15b1e7d3f405bdb2c
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-11 18:56:07 +0300

    Update header-navigation.js

--- assets/js/header-navigation.js
+++ assets/js/header-navigation.js
@@ -21,34 +21,41 @@ function menuShow() {
 }
 
 // Header Navigation
-async function renderBitumHeaderNavigation() {
-    const bitum = await getBitum();
-
-    const logo = document.querySelector('.logo');
-    logo.innerHTML  = `<a href="https://bitum.pp.ua"><img src='./assets/img/new-bitum-logotype.png' alt='Бітум' class='img-logotype'></a>`;
-    logo.innerHTML += `<div style='font-size:12px; position: relative;'>
-                            <span style='position:absolute; top:-10px; left: 2px; color: black; font-weight: bold;letter-spacing:2px; width:200px; filter: drop-shadow(2px 4px 3px white);'>творче об\'єднання</span>
-                       </div>`;
-    logo.innerHTML += `<img src="./assets/img/stop-war-in-ukraine.png" alt="Stop War" class="stop-war">`;
-    
-    const navList = document.querySelector('#navList');
-    navList.innerHTML  =   `<li><a id='titleHome' href='${bitum.navigation.home.linkHome}'>${bitum.navigation.home.titleHome}</a></li>
-                            <li><a id='titleAboutUs' href='${bitum.navigation.aboutUs.linkAboutUs}'>${bitum.navigation.aboutUs.titleAboutUs}</a></li>
-                            <li><a id='titleMusic' href='${bitum.navigation.music.linkMusic}'>${bitum.navigation.music.titleMusic}</a></li>
-                            <li><a id='titleGallery' href='${bitum.navigation.gallery.linkGallery}'>${bitum.navigation.gallery.titleGallery}</a></li>
-                            <li><a id='titleArchive' href='${bitum.navigation.archive.linkArchive}'>${bitum.navigation.archive.titleArchive}</a></li>
-                            <li><a id='titleContacts' href='${bitum.navigation.contacts.linkContacts}'>${bitum.navigation.contacts.titleContacts}</a></li>`;
-    
-    /*
-    let html = '';
-    bitum.navigationMenu.forEach(nav => {
-        let htmlSegment = `<li><a href='${nav.link}'>${nav.title}</a></li>`;
-        html += htmlSegment;
-    });
-    navList.innerHTML = html;
-    */
+const navigationMenu = {
+    "home": {
+        "titleHome": "Головна",
+        "linkHome": "index.html"
+    },
+    "aboutUs": {
+        "titleAboutUs": "Про Нас",
+        "linkAboutUs": "about-us.html"
+    },
+    "music": {
+        "titleMusic": "Музика",
+        "linkMusic": "music.html"
+    },
+    "gallery": {
+        "titleGallery": "Фото",
+        "linkGallery": "gallery.html"
+    },
+    "archive": {
+        "titleArchive": "Архів",
+        "linkArchive": "archive.html"
+    },
+    "contacts": {
+        "titleContacts": "Контакти",
+        "linkContacts": "contacts.html"
+    }
 }
 
-renderBitumHeaderNavigation();
+const logo = document.querySelector('.logo');
+logo.innerHTML  = `<a href="./index.html"><img src='./assets/img/2-new-bitum-logotype.png' alt='Бітум' class='img-logotype'></a>`;
+logo.innerHTML += `<img src="./assets/img/stop-war-in-ukraine.png" alt="Stop War" class="stop-war">`;
 
-
+const navList = document.querySelector('#navList');
+navList.innerHTML  =   `<li><a id='titleHome' href='${navigationMenu.home.linkHome}'>${navigationMenu.home.titleHome}</a></li>
+                        <li><a id='titleAboutUs' href='${navigationMenu.aboutUs.linkAboutUs}'>${navigationMenu.aboutUs.titleAboutUs}</a></li>
+                        <li><a id='titleMusic' href='${navigationMenu.music.linkMusic}'>${navigationMenu.music.titleMusic}</a></li>
+                        <li><a id='titleGallery' href='${navigationMenu.gallery.linkGallery}'>${navigationMenu.gallery.titleGallery}</a></li>
+                        <li><a id='titleArchive' href='${navigationMenu.archive.linkArchive}'>${navigationMenu.archive.titleArchive}</a></li>
+                        <li><a id='titleContacts' href='${navigationMenu.contacts.linkContacts}'>${navigationMenu.contacts.titleContacts}</a></li>`;
