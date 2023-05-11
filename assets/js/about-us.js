0a5d11e1f1e6a8fcf1ec042d5ca3cc345a85bdb6
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-11 20:15:25 +0300

    Update about-us.js

--- assets/js/about-us.js
+++ assets/js/about-us.js
@@ -1,25 +1,18 @@
 'use strict';
 
-async function renderBitumAboutUs() {
-    const bitum = await getBitum();
+document.title = `Творче об'єднання Бітум | Про Нас`;
 
-    document.title = `${bitum.groupName} | ${bitum.navigationMenu[1].title}`;
+// About Us
+const breadcrumbAboutUs = document.querySelector('#breadcrumbAboutUs');
+breadcrumbAboutUs.innerHTML = `<ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="./index.html">Головна </a></li>
+                                    <li class="breadcrumb-item active">Про Нас</li>
+                                </ol>`;
 
-    // About Us
-    const breadcrumbAboutUs = document.querySelector('#breadcrumbAboutUs');
-    breadcrumbAboutUs.innerHTML = `<ol class="breadcrumb">
-                                       <li class="breadcrumb-item"><a href="${bitum.navigation.home.linkHome}">${bitum.navigation.home.titleHome} </a></li>
-                                       <li class="breadcrumb-item active">${bitum.navigation.aboutUs.titleAboutUs}</li>
-                                   </ol>`;
-    
-    const titleMainAboutUs = document.querySelector('#titleMainAboutUs');
-    titleMainAboutUs.innerHTML = `<h1>${bitum.aboutUs.title}</h1>`;
-    
-    const aboutUsContent = document.querySelector('#aboutUsContent');
-    aboutUsContent.innerHTML = `<p class="alert-content">
+const titleMainAboutUs = document.querySelector('#titleMainAboutUs');
+titleMainAboutUs.innerHTML = `<h1>Про Нас</h1>`;
+
+const aboutUsContent = document.querySelector('#aboutUsContent');
+aboutUsContent.innerHTML = `<p class="alert-content">
                                 Історія Бітума зараз в процесі розробки...
-                                </p>`;
- 
-}
-
-renderBitumAboutUs();
+                            </p>`;
