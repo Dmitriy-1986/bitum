5905ef5870619a7a414f296d37fc344f6684608c
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-11 22:03:53 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,1 @@
-const url = 'https://bitum.pp.ua/bitum-database.json';
-
-async function getBitum() {
-    try {
-        let res = await fetch(url);
-        return await res.json();
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+console.log('Творче об\'єднання Бітум');
