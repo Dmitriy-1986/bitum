f7b4bbe4e76d51f867b51e662f6c1f1bcc5a3da6
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-11 21:49:41 +0300

    Update music.js

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,18 +1,33 @@
 'use strict';
 
-async function renderBitumMusic() {
-    const bitum = await getBitum();
+document.title = `Творче об'єднання Бітум | Музика`;
 
-    document.title = `${bitum.groupName} | ${bitum.navigationMenu[2].title}`;
-    
-    const breadcrumbMusic = document.querySelector('#breadcrumbMusic');
-    breadcrumbMusic.innerHTML = `<ol class="breadcrumb">
-                                       <li class="breadcrumb-item"><a href="${bitum.navigation.home.linkHome}">${bitum.navigation.home.titleHome} </a></li>
-                                       <li class="breadcrumb-item active">${bitum.navigation.music.titleMusic}</li>
-                                   </ol>`;
-    
-    const titleMainMusic = document.querySelector('#titleMainMusic');
-    titleMainMusic.innerHTML = `<h1>${bitum.navigation.music.titleMusic}</h1>`;  
-}
+const breadcrumbMusic = document.querySelector('#breadcrumbMusic');
+breadcrumbMusic.innerHTML = `<ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Головна </a></li>
+                                    <li class="breadcrumb-item active">Музика</li>
+                                </ol>`;
 
-renderBitumMusic();
+const titleMainMusic = document.querySelector('#titleMainMusic');
+titleMainMusic.innerHTML = `<h1>Музика</h1>`;  
+      
+/* Btn List or Cards */
+const btnList = document.querySelector('#btnList');
+const cardBlock = document.querySelector('#cardBlock');
+const cards = document.querySelectorAll('.card');
+const cardImg = document.querySelectorAll('.card-img');
+
+btnList.addEventListener('click', () => {
+  btnList.classList.toggle('btn-list');
+
+  cardBlock.classList.toggle('card-col');
+
+  cards.forEach((c) => {
+    c.classList.toggle('card-list-row');
+  });
+
+  cardImg.forEach((img) => {
+    img.classList.toggle('list-img');
+  });
+
+});
