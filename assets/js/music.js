872e7bebbb6f4b01cf6c69dda310200030923545
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-12 19:29:12 +0300

    Update music.js

--- assets/js/music.js
+++ assets/js/music.js
@@ -10,24 +10,3 @@ breadcrumbMusic.innerHTML = `<ol class="breadcrumb">
 
 const titleMainMusic = document.querySelector('#titleMainMusic');
 titleMainMusic.innerHTML = `<h1>Музика</h1>`;  
-      
-/* Btn List or Cards */
-const btnList = document.querySelector('#btnList');
-const cardBlock = document.querySelector('#cardBlock');
-const cards = document.querySelectorAll('.card');
-const cardImg = document.querySelectorAll('.card-img');
-
-btnList.addEventListener('click', () => {
-  btnList.classList.toggle('btn-list');
-
-  cardBlock.classList.toggle('card-col');
-
-  cards.forEach((c) => {
-    c.classList.toggle('card-list-row');
-  });
-
-  cardImg.forEach((img) => {
-    img.classList.toggle('list-img');
-  });
-
-});
