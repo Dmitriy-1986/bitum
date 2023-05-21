40dfffb0279f7ca3f33ddc98b16296a8b8d4d079
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-21 21:30:28 +0300

    Update home-main.js

--- assets/js/home-main.js
+++ assets/js/home-main.js
@@ -85,9 +85,10 @@ contentContacts.innerHTML += `<a href='mailto:${homeMain.contactMain.mail}'>
                                    <img src='./assets/img/icon-gmail.png' alt='Gmail' class='img-gmail'> 
                                    ${homeMain.contactMain.mail}
                               </a>`;
-googleMaps.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d104008.81614515168!2d33.07541334587099!3d48.66423519437349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d09d92b326f807%3A0xe117d4fe41dc0d9!2z0J7Qu9C10LrRgdCw0L3QtNGA0ZbRjywg0JrRltGA0L7QstC-0LPRgNCw0LTRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgMjgwMDE!5e0!3m2!1suk!2sua!4v1681932280382!5m2!1suk!2sua" 
+googleMaps.innerHTML = `<div class="maps"></div>
+                         <!--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d104008.81614515168!2d33.07541334587099!3d48.66423519437349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d09d92b326f807%3A0xe117d4fe41dc0d9!2z0J7Qu9C10LrRgdCw0L3QtNGA0ZbRjywg0JrRltGA0L7QstC-0LPRgNCw0LTRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgMjgwMDE!5e0!3m2!1suk!2sua!4v1681932280382!5m2!1suk!2sua" 
                                      height="350" style="border:0;" allowfullscreen="" loading="lazy" 
-                                    referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+                                    referrerpolicy="no-referrer-when-downgrade"></iframe>-->`;
 
 const mainContent = document.querySelector('#mainContent');
 window.addEventListener('load', () => {
