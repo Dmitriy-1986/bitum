ad626b5cee81d537c636ce39423b579167e13f04
Dovgal Dima <dovgal.dima.86@gmail.com>
2023-06-29 20:26:51 +0300

    First my commit

--- assets/js/scrollDown.js
+++
@@ -1,8 +0,0 @@
-const content = document.querySelector('#content');
-const btnDown = document.querySelector('#btnDown');
-
-function handleButtonClick() {
-    content.scrollIntoView({ block: "start", behavior: "smooth" });
-}
-
-btnDown.addEventListener('click', handleButtonClick);
