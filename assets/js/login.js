8071c2c3c3fabedcf83024dad4789cb4eb965c29
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2024-12-20 03:47:00 +0200

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -20,6 +20,60 @@ btnSubmit.addEventListener('click', (e) => {
       
       result.innerHTML = `<p class="password-valid">Вітаю, ви увійшли на сторінку альбому  
                             <b id="logOut" class="log-out"> | Вийти </b></p>`;
+      result.innerHTML += `
+                         <!-- Audio Player -->
+                          <audio id="myAudio" ontimeupdate="onTimeUpdate()">
+                              <source id="source-audio" src="https://bitum.pp.ua/assets/music/ne-albomnyye/1.Дощі.mp3"
+                                  type="audio/mpeg">
+                              Your browser does not support the audio element.
+                          </audio>
+          
+                          <div class="player-ctn">
+                              <div class="infos-ctn">
+                                  <div class="timer">00:00</div>
+                                  <div class="title"></div>
+                                  <div class="duration">00:00</div>
+                              </div>
+                              <div id="myProgress">
+                                  <div id="myBar"></div>
+                              </div>
+                              <div class="btn-ctn">
+                                  <div class="btn-action first-btn" onclick="previous()">
+                                      <div id="btn-faws-back">
+                                          <i class='fas fa-step-backward'></i>
+                                      </div>
+                                  </div>
+                                  <div class="btn-action" onclick="rewind()">
+                                      <div id="btn-faws-rewind">
+                                          <i class='fas fa-backward'></i>
+                                      </div>
+                                  </div>
+                                  <div class="btn-action" onclick="toggleAudio()">
+                                      <div id="btn-faws-play-pause">
+                                          <i class='fas fa-play' id="icon-play"></i>
+                                          <i class='fas fa-pause' id="icon-pause" style="display: none"></i>
+                                      </div>
+                                  </div>
+                                  <div class="btn-play" onclick="forward()">
+                                      <div id="btn-faws-forward">
+                                          <i class='fas fa-forward'></i>
+                                      </div>
+                                  </div>
+                                  <div class="btn-action" onclick="next()">
+                                      <div id="btn-faws-next">
+                                          <i class='fas fa-step-forward'></i>
+                                      </div>
+                                  </div>
+                                  <div class="btn-mute" id="toggleMute" onclick="toggleMute()">
+                                      <div id="btn-faws-volume">
+                                          <i id="icon-vol-up" class='fas fa-volume-up'></i>
+                                          <i id="icon-vol-mute" class='fas fa-volume-mute' style="display: none"></i>
+                                      </div>
+                                  </div>
+                              </div>
+                              <div class="playlist-ctn"></div>
+                          </div>
+                        `;
       
       const logOut = document.querySelector('#logOut');
       logOut.addEventListener('click', () => {
