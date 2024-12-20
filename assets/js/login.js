31da417c47d4f0828df4b8a3e39d251ca5954079
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2024-12-20 03:36:19 +0200

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,25 +7,27 @@ btnSubmit.addEventListener('click', (e) => {
   e.preventDefault();
   if (password.value.trim() === '') {
     result.innerHTML = '<b class="password-error">Ви не заповнили форму</b>';
-  } else (calcMD5(password.value) === 'a591024321c5e2bdbd23ed35f0574dde') {
-    form.style.display = 'none';
-    document.querySelector('#auth').style.display = 'none';
-    
-    result.innerHTML = `<p class="password-valid">Вітаю, ви увійшли на сторінку альбому  
-                          <b id="logOut" class="log-out"> | Вийти </b></p>`;
-    
-    const logOut = document.querySelector('#logOut');
-    logOut.addEventListener('click', () => {
-        location.reload()
-    });
-    
-    const div = document.createElement('div');
-
-    div.innerHTML += `<div>
-                          <h2>Страница с контентом</h2>
-                      </div>`;  
   } else {
-    result.innerHTML = '<b class="password-error">Ви ввели неправильний пароль</b>';
+    if (calcMD5(password.value) === 'a591024321c5e2bdbd23ed35f0574dde') {
+      form.style.display = 'none';
+      document.querySelector('#auth').style.display = 'none';
+      
+      result.innerHTML = `<p class="password-valid">Вітаю, ви увійшли на сторінку альбому  
+                            <b id="logOut" class="log-out"> | Вийти </b></p>`;
+      
+      const logOut = document.querySelector('#logOut');
+      logOut.addEventListener('click', () => {
+          location.reload()
+      });
+      
+      const div = document.createElement('div');
+
+      div.innerHTML += `<div>
+                            <h2>Страница с контентом</h2>
+                        </div>`;  
+    } else {
+      result.innerHTML = '<b class="password-error">Ви ввели неправильний пароль</b>';
+    }
   }
 });
 
